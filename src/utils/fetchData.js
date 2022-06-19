d7f112781acfdb92d11694829a5c0016cb91ffa3
syrymumbetov <umbetov.syrym@gmail.com>
2022-06-19 15:29:48 +0600

    Footer

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -6,6 +6,15 @@ export const exerciseOptions = {
   },
 };
 
+export const youtubeOptions = {
+  method: 'GET',
+  url: 'https://youtube-search-and-download.p.rapidapi.com/video/related',
+  headers: {
+    'X-RapidAPI-Key': '897b864698mshd9566898364e02dp11e5e2jsn05a7bf77f30a',
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+  },
+};
+
 export const fetchData = async (url, options) => {
   const response = await fetch(url, options);
   const data = await response.json();
